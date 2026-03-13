8234035aa922619049afd5345caf60b1de81081d
Sarah de Paula <vinicius543familia@gmail.com>
2026-03-12 21:29:28 -0300

    hj eu fiz um contador de cliques
    aprendi mto hj

---
+++ Java Script/exx005p2/script.js
@@ -0,0 +1,19 @@
+function acao1() {
+    document.getElementById("mensagens").innerHTML += "<p>Você clicou no primeiro botão</p>";
+}
+
+function acao2() {
+    document.getElementById("mensagens").innerHTML += "<p>Você clicou no segundo botão</p>";
+}
+
+function acao3() {
+    document.getElementById("mensagens").innerHTML += "<p>Você clicou no terceiro botão</p>";
+}
+
+function acao4() {
+    document.getElementById("mensagens").innerHTML += "<p>Você clicou no quarto botão</p>";
+}
+
+function limpar() {
+    document.getElementById("mensagens").innerHTML = "";
+}
